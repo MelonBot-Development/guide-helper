923a332331c7526371f2252b3eab67b90a0e8b0c
Lemon Rose <japandotorg@users.noreply.github.com>
2022-07-22 14:54:05 +0530

    fixed main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "dpp/dpp.h"
+#include "include/config.hpp"
+#include "include/gcommands.hpp"
 
 int main() {
     if (auto config = guide::GuideConfig::load_config()) {
@@ -6,13 +8,16 @@ int main() {
 
         bot.on_log(dpp::utility::cout_logger());
 
+        guide::commands::init_guide(bot);
+
         bot.on_slashcommand([&bot](const dpp:slashCommand_t& event) {
             auto command_name = event.command.get_command_name();
+            guide::commands::handle_slash_command(command_name, event);
         });
 
         bot.on_ready([&bot](const dpp::ready_t& event) {
             if (dpp::run_once<struct register_bot_commands>()) {
-                for (const auto& command)
+                for (const auto& command : guide::commands::slash::commands)
                     bot.global_command_create(command);
             }
         });
