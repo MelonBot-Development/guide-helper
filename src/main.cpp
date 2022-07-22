c28ecae27e5cdb75380ab7e80a6f3e42a0f993d9
Lemon Rose <japandotorg@users.noreply.github.com>
2022-07-22 13:18:05 +0530

    dpp cluster init

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
 #include "dpp/dpp.h"
 
 int main() {
-    if (auto config = )
+    if (auto config = guide::GuideConfig::load_config()) {
+        auto bot = dpp:cluster(config->bot_token, dpp::i_default_intents | dpp::i_message_content);
+
+        bot.on_log(dpp::utility::cout_logger());
+
+        bot.on_slashcommand([&bot](const dpp:slashCommand_t& event) {
+            auto command_name = event.command.get_command_name();
+        });
+
+        bot.on_ready([&bot](const dpp::ready_t& event) {
+            if (dpp::run_once<struct register_bot_commands>()) {
+                for (const auto& command)
+                    bot.global_command_create(command);
+            }
+        });
+
+        bot.start(false);
+    } else {
+        fmt::print("Failed to open config.")
+    }
+
+    return 0;
 }
